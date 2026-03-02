f6f8cef3a34ffe33eb520fc3df85b14dd0bc7677
OliwerWesther <Oliwer.westher@elev.ga.lbs.se>
2026-03-02 13:54:33 +0100

    point manager

---
+++ Game jam-26, group 1/Assets/_Oliwer/Scripts Oliwer/PointManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+    [Header("Points")]
+    public int _CurrentPoints = 0;
+    public int _rewardingPoints = 10;
+
+    void Update()
+    {
+        Health[] allHealthScripts = FindObjectsOfType<Health>();
+
+        foreach (Health h in allHealthScripts)
+        {
+            if (h._health <= 0 && !h._isDead)
+            {
+                _CurrentPoints += _rewardingPoints;
+                h._isDead = true;
+
+                Debug.Log("Quest Completed for: " + h.name);
+            }
+        }
+    }
+}
